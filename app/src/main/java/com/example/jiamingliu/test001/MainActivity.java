963fc2d47cc016427649b231a4a5b95922c6a6d6
liujiaminggit <865090421@qq.com>
2016-11-25 17:45:30 +0800

    test002

--- app/src/main/java/com/example/jiamingliu/test001/MainActivity.java
+++ app/src/main/java/com/example/jiamingliu/test001/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.jiamingliu.test001;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView layout=(TextView)findViewById(R.id.tvPWD);
+        layout.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        int x=(int)motionEvent.getX();
+        int y=(int)motionEvent.getY();
+        Toast.makeText(this,"触摸屏幕位置为:(" + x + "," + y + ")", Toast.LENGTH_LONG).show();
+        return true;
+
     }
 }
